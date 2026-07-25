da6596337a5e7dce4c87f9ff8109826e293a4a37
drblack001 <21rajatsharma98@gmail.com>
2026-07-25 21:53:00 +0530

    Time: 0 ms (100%), Space: 9.1 MB (51.27%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
     string generateTheString(int n) {
-        string s="";
-        int n1=n;
-        if(n%2==0){
-            int i=0;
-            while(n--){
-                if(i==n1-1){
-                    s+="b";
-                }
-                else{
-                s+="a";
-                }
-                i++;
-            }
-        }
-        else{
-            int i=0;
-            while(n--){
-                s+="a";
-                i++;
-            }
-        }
-        return s;
+        if(n % 2 == 1)
+            return string(n, 'a');
+        return string(n-1, 'a') + "b";
     }
 };

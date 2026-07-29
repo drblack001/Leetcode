6f343694db14f40924bfb899ebb1869b67df663b
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:27:33 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/29/2026, 2:27:34 PM
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+          int m1[256] = {0}, m2[256] = {0}; 
+          
+          int n = s.size(); 
+          for (int i = 0; i < n; ++i) {
+              if (m1[s[i]] != m2[t[i]]) return false;
+              m1[s[i]] = i + 1;
+              m2[t[i]] = i + 1;
+          }
+          
+          return true;
+        
+    }
+};

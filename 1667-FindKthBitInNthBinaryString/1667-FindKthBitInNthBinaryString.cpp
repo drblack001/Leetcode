e4b4bd00c5e3bd94bcfa3e1eb9d12ab7aa49cf4f
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:26:33 +0530

    Create 1667-FindKthBitInNthBinaryString

---
+++ 1667-FindKthBitInNthBinaryString/1667-FindKthBitInNthBinaryString.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/29/2026, 2:26:33 PM
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if(n == 1)
+            return '0';
+        
+        int length = (1 << n) - 1;   
+        int mid = (length / 2) + 1;
+        
+        if(k == mid)
+            return '1';
+        if(k < mid)
+            return findKthBit(n - 1, k);
+        char bit = findKthBit(n - 1, length - k + 1);
+        
+        return (bit == '0') ? '1' : '0';
+    }
+};

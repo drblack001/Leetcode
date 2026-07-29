bc2454df8ddb9ef9bc6cd0da408bf2fe844850a3
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:26:40 +0530

    Create 1482-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ 1482-HowManyNumbersAreSmallerThanTheCurrentNumber/1482-HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/29/2026, 2:26:41 PM
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> n;
+        for(int i =0;i<nums.size();i++){
+            int count =0;
+            for (int j =0 ;j<nums.size();j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }                
+            }
+            n.push_back(count);
+        }
+        return n;
+    }
+};

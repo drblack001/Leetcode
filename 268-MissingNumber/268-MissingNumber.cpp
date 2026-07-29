dce9b71c3a1fdc836dfb7ddc0965cab018cd2ab7
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:27:21 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/29/2026, 2:27:21 PM
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();    
+        int sum = n * (n + 1) / 2; 
+        int sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum2 += nums[i];
+        }
+        
+        return sum - sum2;
+    }
+};

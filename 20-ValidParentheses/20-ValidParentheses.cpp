848b3e638d2eb848d91d47cd6db12a9147a3758c
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:28:15 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/29/2026, 2:28:16 PM
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i =0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+            st.push(s[i]);
+            else{
+                if(st.size()==0)
+                return false;
+                
+                if(st.top()=='('&& s[i]==')'||
+                  st.top()=='['&& s[i]==']'||
+                  st.top()=='{'&& s[i]=='}')
+                  st.pop();
+                else
+                return false;
+            }
+            
+        }
+        return st.empty();
+    }
+};

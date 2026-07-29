1649898bc76a0ffd2c9dcb48a5de0a4148e014c4
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:26:50 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/29/2026, 2:26:51 PM
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int duplicate = -1, missing = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+
+            if (nums[index] < 0) {
+                duplicate = abs(nums[i]);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        return {duplicate, missing};
+    }
+};

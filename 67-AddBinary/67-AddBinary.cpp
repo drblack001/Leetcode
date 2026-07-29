d1a4d8c86ed0e5be971b49d1e34ac03fcaf85ee0
drblack001 <21rajatsharma98@gmail.com>
2026-07-29 14:27:57 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/29/2026, 2:27:58 PM
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if(i >= 0) sum += (a[i--] - '0');
+            if(j >= 0) sum += (b[j--] - '0');
+
+            result = char((sum % 2) + '0') + result;
+            carry = sum / 2;
+        }
+
+        return result;
+    }
+};

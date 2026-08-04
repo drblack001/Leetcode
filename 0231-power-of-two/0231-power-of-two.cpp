7cb622ebbbc5c762617cb6cbe5e04c5325598d92
drblack001 <21rajatsharma98@gmail.com>
2026-08-04 21:23:14 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.62%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        double x = log2(n);          
+        return floor(x) == ceil(x);
+    }
+};

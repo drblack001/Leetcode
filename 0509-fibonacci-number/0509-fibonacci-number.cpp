0c798c65528894ec62e1c098722eab1204dc4280
drblack001 <21rajatsharma98@gmail.com>
2026-08-08 20:53:35 +0530

    Time: 2 ms (77.79%), Space: 7.7 MB (65.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==1)return 1;
-        if(n==0) return 0;
-        return fib(n-1)+fib(n-2);
+
+        if(n<=1)
+        return n;
+
+        int a=0,b=1,c;
+        for(int i =2;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+        
     }
 };

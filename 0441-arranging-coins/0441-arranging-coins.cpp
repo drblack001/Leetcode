367d784accb7a1c76c0c3eb4eea8ebe8bd368beb
drblack001 <21rajatsharma98@gmail.com>
2026-08-10 12:03:27 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.75%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int row = 1;
-        int count = 0;
-
-        while (n >= row) {
-            n -= row;
-            count++;
-            row++;
-        }
-
-        return count;
+        return (sqrt(1 + 8LL * n) - 1) / 2;
     }
 };

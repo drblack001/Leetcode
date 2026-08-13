edb4b8d2a47237d26893e2c0a5e359fb6ba3e9fa
drblack001 <21rajatsharma98@gmail.com>
2026-08-13 21:26:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (93.55%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,11 +4,16 @@ public:
         double ans = 1;
         double ans1 = 1;
         long long a = n;
+
+        if(x == 1 || x == -1 && n % 2 == 0)
+            return 1;
+
         if(n > 0){
             while(a > 0){
                 if(a % 2 == 1){
                     ans = ans * x;
                 }
+
                 x = x * x;
                 a = a / 2;
             }
@@ -20,13 +25,16 @@ public:
         else{
             a = -a;
             x = 1 / x;
+
             while(a > 0){
                 if(a % 2 == 1){
                     ans1 = ans1 * x;
                 }
+
                 x = x * x;
                 a = a / 2;
             }
+
             return ans1;
         }
     }

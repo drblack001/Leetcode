a6a1dbb51972c6d831f08e9aed51fc3a757f072b
drblack001 <21rajatsharma98@gmail.com>
2026-08-13 21:15:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        double ans1 = 1;
+        long long a = n;
+        if(n > 0){
+            while(a > 0){
+                if(a % 2 == 1){
+                    ans = ans * x;
+                }
+                x = x * x;
+                a = a / 2;
+            }
+            return ans;
+        }
+        else if(n == 0){
+            return 1;
+        }
+        else{
+            a = -a;
+            x = 1 / x;
+            while(a > 0){
+                if(a % 2 == 1){
+                    ans1 = ans1 * x;
+                }
+                x = x * x;
+                a = a / 2;
+            }
+            return ans1;
+        }
+    }
+};

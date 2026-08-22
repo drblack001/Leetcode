78d8995918c002685805b5a3db60113178165f32
drblack001 <21rajatsharma98@gmail.com>
2026-08-22 21:27:28 +0530

    Time: 17 ms (53.12%), Space: 26.1 MB (51.27%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<int, vector<char>> mp;
+        unordered_map<string, vector<string>> mp;
         for (int i = 0; i < strs.size(); i++) {
-            for (int j = 0; j < strs[i].size(); j++) {
-                mp[i].push_back(strs[i][j]);
-            }
-            sort(mp[i].begin(), mp[i].end());
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(strs[i]);
         }
-
         vector<vector<string>> ans;
-        vector<bool> visited(strs.size(), false);
-
-        for (int i = 0; i < strs.size(); i++) {
-            if (visited[i]) {
-                continue;
-            }
-            vector<string> temp;
-            temp.push_back(strs[i]);
-            visited[i] = true;
-            for (int j = i + 1; j < strs.size(); j++) {
-                if (visited[j]) {
-                    continue;
-                }
-                if (mp[i] == mp[j]) {
-                    temp.push_back(strs[j]);
-                    visited[j] = true;
-                }
-            }
-
-            ans.push_back(temp);
+        for (auto it : mp) {
+            ans.push_back(it.second);
         }
-
         return ans;
     }
 };

ecb13a4bce87561b8c29848e8cf385f74ca458b3
drblack001 <21rajatsharma98@gmail.com>
2026-08-29 20:37:52 +0530

    Time: 0 ms (100%), Space: 14.3 MB (6.46%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> st;
+        stack<int> st;
 
         for(int i=0;i<prices.size();i++){
-            int j=i+1;
-
-            while(j<prices.size()){
-                if(prices[i]>=prices[j]){
-                    st.push_back(prices[i]-prices[j]);
-                    break;
-                }
-                else{
-                    j++;
-                }
+            while(!st.empty() && prices[st.top()] >= prices[i]){
+                prices[st.top()] = prices[st.top()] - prices[i];
+                st.pop();
             }
-
-            if(j==prices.size()){
-                st.push_back(prices[i]);
-            }
+            st.push(i);
         }
 
-        return st;
+        return prices;
     }
 };

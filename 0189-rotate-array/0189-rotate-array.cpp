dad4ab9c95a22aa8c7934a8106945830814a4afc
drblack001 <21rajatsharma98@gmail.com>
2026-09-15 21:13:13 +0530

    Time: 2 ms (42.45%), Space: 261.7 MB (54.45%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == 1) return;
+        k = k % n;
+        vector<int> first(nums.begin(), nums.end() - k);
+        vector<int> second(nums.begin() + n - k, nums.end());
+        second.insert(second.end(), first.begin(), first.end());
+        nums = second;
+    }
+};

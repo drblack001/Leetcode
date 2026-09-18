d38ca212fac473acd09162a1817e32bbed1205fa
drblack001 <21rajatsharma98@gmail.com>
2026-09-18 21:17:29 +0530

    Time: 63 ms (7.41%), Space: 60.7 MB (6.7%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
-        int k = piles.size() - ( piles.size()/3);
+        // int k = piles.size() - ( piles.size()/3);
+        int k = 2*( piles.size()/3);
         sort(piles.rbegin(), piles.rend());
         int ans=0;
         for(int i=1;i<=k;i+=2){

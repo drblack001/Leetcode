3c699eeab6510bd562c889d7be14e8cef30467f5
drblack001 <21rajatsharma98@gmail.com>
2026-09-25 14:00:18 +0530

    Time: 0 ms (100%), Space: 9.7 MB (61.11%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        // ans[0].push_back(1); 
-        // ans[1].push_back(1);
-        // ans[1].push_back(1);
-        // int i=1;
-        // while(i<numRows){
-        //     vector<int> temp;
-        //     int j=0;
-        //     temp.push_back(1);
-        //     while(j<i){
-        //         int n = ans[i][j]+ans[i][j+1];
-        //         temp.push_back(n);
-        //         j++;
-        //     }
-        //     temp.push_back(1);
-        //     ans.push_back(temp);
-        //     i++;
-        // }
-        for(int i=0;i<numRows;i++){
-            vector<int>temp(i+1,1);
-            for(int j=1;j<i;j++){
-                temp[j] = ans[i-1][j-1]+ans[i-1][j];
+        if (numRows <= 0) return ans;
+        ans.push_back({1});
+        int i=1;
+        while(i<numRows){
+            vector<int> temp;
+            int j = 0;
+            temp.push_back(1);
+            while (j < i-1) {
+                int n = ans[i-1][j] + ans[i-1][j + 1];
+                temp.push_back(n);
+                j++;
             }
+            temp.push_back(1);
             ans.push_back(temp);
+            i++;
         }
+        // for(int i=0;i<numRows;i++){
+        //     vector<int>temp(i+1,1);
+        //     for(int j=1;j<i;j++){
+        //         temp[j] = ans[i-1][j-1]+ans[i-1][j];
+        //     }
+        //     ans.push_back(temp);
+        // }
         return ans;
     }
 };

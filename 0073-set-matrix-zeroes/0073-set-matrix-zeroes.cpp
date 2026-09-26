4dfca206ae9deae80f4da1b1183d4076e659719e
drblack001 <21rajatsharma98@gmail.com>
2026-09-26 21:00:54 +0530

    Time: 5 ms (11.84%), Space: 21.3 MB (9.62%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int,bool> row;
-        unordered_map<int,bool> cols;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0){
-                    row[i]=true;
-                    cols[j]=true;
-                }
-            }
-        }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(row[i] || cols[j]){
-                    matrix[i][j]=0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        set<int> rows;
+        set<int> cols;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
                 }
             }
         }
 
+        for (int i : rows) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        for (int j : cols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][j] = 0;
+            }
+        }
     }
 };
